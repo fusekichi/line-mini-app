49e12cf68289b852981e375ca56005edab27a706
fusekichi <yuta0120qawsed@gmail.com>
2025-05-01 05:10:02 +0900

    prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,24 +1,26 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-// PrismaClientインスタンス化関数
-function getPrismaClient() {
-  // ビルド時は空のモックを返す
-  if (process.env.NODE_ENV === 'production' && process.env.NEXT_PHASE === 'phase-production-build') {
-    console.log('ビルド時はPrismaクライアントをモック化します');
-    // @ts-ignore
-    return {}
-  }
+// // PrismaClientインスタンス化関数
+// function getPrismaClient() {
+//   // ビルド時は空のモックを返す
+//   if (process.env.NODE_ENV === 'production' && process.env.NEXT_PHASE === 'phase-production-build') {
+//     console.log('ビルド時はPrismaクライアントをモック化します');
+//     // @ts-ignore
+//     return {}
+//   }
   
-  // 通常の場合はPrismaClientを返す
-  const globalForPrisma = global as unknown as { prisma: PrismaClient }
-  const prisma = globalForPrisma.prisma || new PrismaClient()
+//   // 通常の場合はPrismaClientを返す
+//   const globalForPrisma = global as unknown as { prisma: PrismaClient }
+//   const prisma = globalForPrisma.prisma || new PrismaClient()
   
-  if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+//   if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
   
-  return prisma
-}
+//   return prisma
+// }
 
-export default getPrismaClient()
+// export default getPrismaClient()
+
+
 
 // import { PrismaClient } from '@prisma/client';
 
@@ -37,8 +39,8 @@ export default getPrismaClient()
 
 
 
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
-// export default prisma
+export default prisma
