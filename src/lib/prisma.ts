ccd9e3f6b1c9beab0984c4868a239a2671d2fa4a
fusekichi <yuta0120qawsed@gmail.com>
2025-05-01 04:41:47 +0900

    prisma 簡素化

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,19 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-// グローバル変数の型拡張
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+// // グローバル変数の型拡張
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
 
-// 開発環境でホットリロード時に複数のPrismaインスタンスが作成されるのを防ぐ
-const prisma = global.prisma || new PrismaClient();
+// // 開発環境でホットリロード時に複数のPrismaインスタンスが作成されるのを防ぐ
+// const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+// if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
-export default prisma;
+// export default prisma;
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default prisma
