aa367469039bf9367e4cd45d92995f006b93a8bc
fusekichi <yuta0120qawsed@gmail.com>
2025-05-01 04:46:37 +0900

    一時対応

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,25 @@
+import { PrismaClient } from '@prisma/client'
+
+// PrismaClientインスタンス化関数
+function getPrismaClient() {
+  // ビルド時は空のモックを返す
+  if (process.env.NODE_ENV === 'production' && process.env.NEXT_PHASE === 'phase-production-build') {
+    console.log('ビルド時はPrismaクライアントをモック化します');
+    // @ts-ignore
+    return {}
+  }
+  
+  // 通常の場合はPrismaClientを返す
+  const globalForPrisma = global as unknown as { prisma: PrismaClient }
+  const prisma = globalForPrisma.prisma || new PrismaClient()
+  
+  if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+  
+  return prisma
+}
+
+export default getPrismaClient()
+
 // import { PrismaClient } from '@prisma/client';
 
 // // グローバル変数の型拡張
@@ -12,8 +34,11 @@
 
 // export default prisma;
 
-import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
 
-export default prisma
+
+// import { PrismaClient } from '@prisma/client'
+
+// const prisma = new PrismaClient()
+
+// export default prisma
